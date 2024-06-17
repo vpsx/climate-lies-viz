e7b34c115dbfdf7c5ba350fabda7d7ebd4ce67d9
Zoe <19900057+vpsx@users.noreply.github.com>
2024-06-17 14:23:13 -0400

    Update base - vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // will need to remove this if then we set up GH pages with a custom domain
+  base: "/climate-lies-viz/",
 });
